b512a1c92bb61558c7049734bde492e367adea6f
Tyler Long <tyler4long@gmail.com>
2022-02-22 14:28:40 -0800

    Cannot reproduce the issue

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,15 @@ const node = document.createElement('h1');
 node.innerText = 'Hello world!';
 document.body.appendChild(node);
 
+const button = document.createElement('button');
+button.innerText = 'Click me';
+document.body.appendChild(button);
+button.onclick = () => {
+  main();
+};
+
 const rc = new RingCentral({
-  clientId: 'fake',
+  clientId: 'jJxc4f1ASJ2JlizgMdNnvg',
   clientSecret: 'fake',
   server: 'https://platform.ringcentral.com',
 });
@@ -13,4 +20,4 @@ const rc = new RingCentral({
 const main = async () => {
   await rc.authorize({username: 'fake', extension: '', password: 'fake'});
 };
-main();
+// main();
